a3de5cb43225ac40be96498b418c6bfefb0fae5b
Sergio Rivas <sergiorivas@gmail.com>
2025-10-19 09:48:06 -0700

    Update Homebrew formula to v1.0.6

--- Formula/git-shade.rb
+++ Formula/git-shade.rb
@@ -1,16 +1,16 @@
 class GitShade < Formula
   desc "CLI tool that maintains a single unified Git repository for all your project's excluded files (configs, secrets, large files)"
   homepage "https://sergiorivas@github.com/sergiorivas/git-shade"
-  version "1.0.5"
+  version "1.0.6"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/sergiorivas/git-shade/releases/download/v1.0.5/git-shade-v1.0.5-x86_64-apple-darwin.tar.gz"
-      sha256 "e3997241906e528b57a2df88c202846f9506f75230997faaa9949b351b012bc3"
+      url "https://github.com/sergiorivas/git-shade/releases/download/v1.0.6/git-shade-v1.0.6-x86_64-apple-darwin.tar.gz"
+      sha256 "f86e3a55642f3bbd7225ba168b7bba8103b0b109207f0aefda64700d08f890cd"
     else
-      url "https://github.com/sergiorivas/git-shade/releases/download/v1.0.5/git-shade-v1.0.5-aarch64-apple-darwin.tar.gz"
-      sha256 "ff59f274e1d4f588b7c5b3ff1b797f1f02ba493e8c601eeed02ab219a12ca3bc"
+      url "https://github.com/sergiorivas/git-shade/releases/download/v1.0.6/git-shade-v1.0.6-aarch64-apple-darwin.tar.gz"
+      sha256 "058c77805013874d40ccb10d01ce6e213da7f00aad59646377e4c78d149dfbde"
     end
   end
 
